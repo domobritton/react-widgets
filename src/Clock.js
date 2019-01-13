15b5e25f3c2f0ebbe8fd42bb300d5e0a74e74c11
domobritton <domonicbritton@gmail.com>
2019-01-13 13:19:32 -0800

    Refine style on clock

--- src/Clock.js
+++ src/Clock.js
@@ -38,9 +38,9 @@ export default class Clock extends Component {
         if (stdHours === 0) { stdHours = 12; }
         return (
             <Time>
-                <Hour>{`${stdHours} : `}</Hour>
-                <Minute>{minutes < 10 ? `0${minutes} : ` : `${minutes} : `}</Minute>
-                <Second>{seconds < 10 ? `0${seconds}` : seconds}</Second> 
+                <Hour>{`${stdHours}`}</Hour>
+                <Minute>{minutes < 10 ? ` : 0${minutes}` : ` : ${minutes}`}</Minute>
+                <Second>{seconds < 10 ? ` : 0${seconds}` : ` : ${seconds}`}</Second> 
                 <AMPM>{amPm}</AMPM>
             </Time> 
         )    
@@ -50,9 +50,9 @@ export default class Clock extends Component {
         const { hours, minutes, seconds } = this.state;
         return (
             <Time>
-                <Hour>{hours < 10 ? `0${hours} : ` : `${hours} : ` }</Hour>
-                <Minute>{minutes < 10 ? `0${minutes} : ` : `${minutes} : `}</Minute>
-                <Second>{seconds < 10 ? `0${seconds}` : seconds}</Second>
+                <Hour>{hours < 10 ? `0${hours}` : `${hours}` }</Hour>
+                <Minute>{minutes < 10 ? ` : 0${minutes}` : `  : ${minutes}`}</Minute>
+                <Second>{seconds < 10 ? ` : 0${seconds}` : ` : ${seconds}`}</Second>
             </Time>
         )
     }
@@ -61,52 +61,76 @@ export default class Clock extends Component {
       const { military } = this.state;
 
       return (
-          <Wrapper>
-              {military ? this.clock() : this.twelveHourClock()}
+        <>
+            <TimeWrapper>
+                {military ? this.clock() : this.twelveHourClock()}
+            </TimeWrapper>
             <Button 
-            onClick={this.handleClick}>
+                onClick={this.handleClick}>
                 {military ? `12 hour` : `24 hour`}
             </Button>
-          </Wrapper>
+        </>
       )
   }
 }
 
-const Wrapper = styled.div`
+const TimeWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    height: 80vh;
+    background: #159957;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #155799, #159957);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #155799, #159957); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    height: 90vh;
 `;
 
 const Time = styled.div`
-    font-size: 120px;
+    font-size: 150px;
     color: darkgray;
     display: flex;
+    justify-content: space-around;
 `;
 
 const Button = styled.button`
+    position: absolute;
+    bottom: 50px;
+    right: 40px;
     padding: 10px;
-    background: lightblue;
-    border: 2px solid lightblue;
+    background: #155799;
+    border: 2px solid #155799;
     border-radius: 5px;
     font-size: 18px;
     font-weight: 900;
     color: white;
     outline: none;
     width: 120px;
+    cursor: pointer;
+    transition: all 250ms ease-in-out;
+
+    &:hover {
+        background: #1B5B96;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    }
 `;
 
-const Hour = styled.div`
+const Text = styled.div`
+    text-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    flex: 0 0 275px;
+    display: block;
 `;
 
-const Minute = styled.div`
+const Hour = styled(Text)`
+    text-align: right;
+    padding-right: 25px;
 `;
 
-const Second = styled.div`
+const Minute = styled(Text)`
 `;
 
-const AMPM = styled.div`
-    
+const Second = styled(Text)`
+`;
+
+const AMPM = styled(Text)`
+    font-size: 80px;
 `;
