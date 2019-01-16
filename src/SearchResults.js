6e82d2ad57284fd505c476a8ade2a9ee443c568b
domobritton <domonicbritton@gmail.com>
2019-01-16 00:03:52 -0800

    Add search results

---
+++ src/SearchResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am your search results.</h1>
+      </div>
+    )
+  }
+}
